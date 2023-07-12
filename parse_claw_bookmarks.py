d0072b18f7756c498286f924f69a88973087de42
Dipesh Kafle <dipesh.kaphle111@gmail.com>
2023-07-12 11:07:23 +0530

    fix empty url issue

--- parse_claw_bookmarks.py
+++ parse_claw_bookmarks.py
@@ -17,6 +17,8 @@ with open("static/claw-export.json", "r") as f:
     for row in json:
         out.write(
             """- [{}]({}) , [Lobste.rs Discussion]({})\n""".format(
-                row["title"], row["url"], row["comments_url"]
+                row["title"],
+                row["url"] if row["url"] != "" else row["comments_url"],
+                row["comments_url"],
             )
         )
